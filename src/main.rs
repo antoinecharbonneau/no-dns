b6ff9640bd80a33079796e95d0e02f3b9fbbed58
Antoine Charbonneau <antoine@charbonneau.dev>
2023-05-11 13:24:46 -0400

    Added more starting informations

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod dns;
 mod cli;
 mod blocklist;
-use log::info;
 use env_logger;
 use dns::server;
 
 fn main() {
     env_logger::init();
-    info!("Starting NO DistractioNS");
+    log::info!("Starting No DNS on {}", cli::Args::get_params().get_bind().to_string());
     server::dispatcher::start();
 }
