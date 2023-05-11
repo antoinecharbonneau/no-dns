911840a7ffb91563a665353f0c7e29cadc3b79c6
Antoine Charbonneau <antoine@charbonneau.dev>
2023-05-11 13:08:31 -0400

    Added * pattern matching to blocklist

--- src/blocklist/file.rs
+++ src/blocklist/file.rs
@@ -9,11 +9,44 @@ pub fn is_blocked(domain: &Name) -> bool {
 
     match handle {
         Ok(blocklist) => {
-            return blocklist.split("\n").any(|d| d == domain.to_string());
+            match_blocked(&blocklist, &domain.to_string())
         },
         Err(_) => {
             log::error!("File list not available, no filtering will be possible");
             return false;
         },
+    }
+}
+
+fn match_blocked(blocklist: &String, domain: &str) -> bool {
+    let domain_lower = domain.to_lowercase();
+    return blocklist.split("\n").any(|d: &str| {
+        let d = &d.trim().to_lowercase();
+        // exact match
+        return d == &domain_lower
+        // patern match (*.example.com)
+        || d.starts_with("*.") && domain_lower.ends_with(&d[2..d.len()])
+    });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_match_blocked() {
+        let blocklist = String::from("
+        youtube.com
+        google.com
+        *.test.ca
+        www.eXample.com
+        ");
+
+        assert!(match_blocked(&blocklist, "youtube.com"));
+        assert!(!match_blocked(&blocklist, "www.google.com"));
+        assert!(match_blocked(&blocklist, "test2.test.ca"));
+        assert!(match_blocked(&blocklist, "test.ca"));
+        assert!(!match_blocked(&blocklist, "test.ca.google.com"));
+        assert!(match_blocked(&blocklist, "www.example.com"));
     }
 }
